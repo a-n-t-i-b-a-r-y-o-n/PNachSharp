79001a03f50d98efb93eb74f5a8f2172b22fea8d
a-n-t-i-b-a-r-y-o-n <77591737+a-n-t-i-b-a-r-y-o-n@users.noreply.github.com>
2021-07-17 19:25:50 -0500

    Rust FFI PoC based off example

--- PNachSharp/RightPane.xaml.cs
+++ PNachSharp/RightPane.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace PNachSharp
 {
@@ -7,6 +8,11 @@ namespace PNachSharp
         public RightPane()
         {
             InitializeComponent();
+        }
+
+        private void CopyButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            OutputBox.Text = RustFFI.SumOfEven(new[] {1, 2, 3, 4}) + "";
         }
     }
 }
---
+++ PNachSharp/RustFFI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PNachSharp
+{
+    class RustFFI
+    {
+        [DllImport(@"ffi.dll")]
+        private static extern uint sum_of_even(int[] n, UIntPtr len);
+
+        public static uint SumOfEven(int[] n)
+        {
+            return sum_of_even(n, (UIntPtr)n.Length);
+        }
+    }
+}
